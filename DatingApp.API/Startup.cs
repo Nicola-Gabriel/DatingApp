4c465193c42d333064050f2d1291b430d8a2d5a8
Nicola-Gabriel <gabriel.nicola.00@gmail.com>
2020-09-09 11:58:24 +0300

    Action Filters

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -41,6 +41,7 @@ namespace DatingApp.API
             services.AddAutoMapper();
             services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddCors();
+            services.AddScoped<LogUserActivity>();
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
